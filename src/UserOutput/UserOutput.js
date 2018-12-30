47f23d75fc5f87dc406ab961640906ef715e9611
Nate Baer <njbbaer@gmail.com>
2018-12-30 02:14:18 -0800

    Create UserOutput component

---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const userOutput = ( props ) => {
+    return (
+        <div>
+            <p>Usernme: njbbaer</p>
+            <p>This is a UserOutput component</p>
+        </div>
+    )
+};
+
+export default userOutput
