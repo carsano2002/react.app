4ee0da0f78854491cc9e402c6d6ba63a61f8936b
Nate Baer <njbbaer@gmail.com>
2018-12-30 02:31:16 -0800

    Fix console errors

--- src/App.js
+++ src/App.js
@@ -9,11 +9,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            <UserOutput />
-            <UserOutput />
-            <UserOutput />
-          </p>
+          <UserOutput />
+          <UserOutput />
+          <UserOutput />
         </header>
       </div>
     );
